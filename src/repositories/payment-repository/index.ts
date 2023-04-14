630b5bdb5e51306bba25309bf4fc1891493a79be
Schweidzon <dannielsch@gmail.com>
2023-04-14 20:20:57 -0300

    fix: delete unsed import

--- src/repositories/payment-repository/index.ts
+++ src/repositories/payment-repository/index.ts
@@ -1,10 +1,10 @@
 import { prisma } from '@/config';
 import { PaymentInput } from '@/protocols';
-import { Payment } from '@prisma/client';
+
 
 async function payTicket(payment: PaymentInput) {
     return prisma.payment.create({
-        data:payment 
+        data: payment
     })
 }
 
