f66e89df7ad7a10f16ab32d9a07af1eb0250facb
Schweidzon <dannielsch@gmail.com>
2023-04-15 10:33:32 -0300

    fix: paymento controller

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -3,49 +3,49 @@ import { PaymentInfo } from "@/protocols";
 import paymentService from "@/services/payment-service";
 import ticketsService from "@/services/tickets-service";
 import { Payment } from "@prisma/client";
-import { NextFunction, Response } from "express";
+import { Response } from "express";
 import { ApplicationError } from '@/protocols';
 
 
 export async function payTicket(req: AuthenticatedRequest, res: Response) {
     const userId = req.userId
     const paymentInfo = req.body as PaymentInfo
-    
+
     try {
         const payment = await paymentService.payTicket(userId, paymentInfo)
         console.log(payment)
-        
+
         return res.send(payment)
-    } catch (error ) {
+    } catch (error) {
         const err = error as ApplicationError
-        
-      
-        if(err.name === 'NotFoundError') {
+
+
+        if (err.name === 'NotFoundError') {
             return res.status(404).send(err)
-        } 
-        if(err.name === 'UnauthorizedError') {
+        }
+        if (err.name === 'UnauthorizedError') {
             return res.send(401).send(err)
         }
     }
-    
+
 }
 
 export async function getPaymentInfo(req: AuthenticatedRequest, res: Response) {
-   const ticketId = Number(req.query.ticketId)
-   const userId = req.userId  
-   try {
-    const paymentInfo = await paymentService.getPaymentInfo(ticketId, userId)
-    return res.send(paymentInfo)
-   } catch (error) {
-    const err = error as ApplicationError
-    if(err.name === 'NotFoundError') {
-        return res.status(404).send(err)
-    } 
-    if(err.name === 'RequestError') {
-        return res.status(400).send(err)
+    const ticketId = Number(req.query.ticketId)
+    const userId = req.userId
+    try {
+        const paymentInfo = await paymentService.getPaymentInfo(ticketId, userId)
+        return res.send(paymentInfo)
+    } catch (error) {
+        const err = error as ApplicationError
+        if (err.name === 'NotFoundError') {
+            return res.status(404).send(err)
+        }
+        if (err.name === 'RequestError') {
+            return res.status(400).send(err)
+        }
+        if (err.name === 'UnauthorizedError') {
+            return res.status(401).send(err)
+        }
     }
-    if(err.name === 'UnauthorizedError') {
-        return res.send(401).send(err)
-    }
-   }
 }
